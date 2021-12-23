f0a3ae6ac877a9af9031014a3a35aea40070cf62
Hamish Aman Prakash <83937512+A-TechDS@users.noreply.github.com>
2021-12-23 22:43:17 +0000

    Alt tags added

--- pages/my-assets.js
+++ pages/my-assets.js
@@ -54,7 +54,7 @@ export default function MyAssets() {
           {
             nfts.map((nft, i) => (
               <div key={i} className="border shadow rounded-xl overflow-hidden">
-                <Image src={nft.image} className="rounded" />
+                <Image src={nft.image} className="rounded" alt="" />
                 <div className="p-4 bg-black">
                   <p className="text-2xl font-bold text-white">Price - {nft.price} Eth</p>
                 </div>
